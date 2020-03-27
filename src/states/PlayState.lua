a9244046719f5a614773160e39f898f25e2ff2d1
RyanSMac <ryanmcconville93@gmail.com>
2020-03-27 16:00:11 +0000

    Breakout: Victory Update
    Added victory state which display info about the level just beated and sets up the nest level

--- src/states/PlayState.lua
+++ src/states/PlayState.lua
@@ -22,6 +22,7 @@ function PlayState:enter(params)
     self.health = params.health
     self.score = params.score
     self.ball = params.ball
+    self.level = params.level
 
     -- give ball random starting velocity
     self.ball.dx = math.random(-200, 200)
@@ -78,6 +79,19 @@ function PlayState:update(dt)
 
             -- trigger brick hit function
             brick:hit()
+
+            -- go to our victory screen if there are no more bricks left
+            if self:checkVictory() then
+                gSounds['victory']:play()
+
+                gStateMachine:change('victory', {
+                    level = self.level,
+                    paddle = self.paddle,
+                    health = self.health,
+                    score = self.score,
+                    ball = self.ball,
+                })
+            end
 
             --
             -- collision code for bricks
@@ -176,4 +190,14 @@ function PlayState:render()
         love.graphics.setFont(gFonts['large'])
         love.graphics.printf("PAUSED", 0, VIRTUAL_HEIGHT / 2 - 16, VIRTUAL_WIDTH, 'center')
     end
+end
+
+function PlayState:checkVictory()
+    for k, brick in pairs(self.bricks) do
+        if brick.inPlay then
+            return false
+        end 
+    end
+
+    return true
 end
